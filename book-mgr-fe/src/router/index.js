d7bcb49389c6d639219e690c168e337dc2aa4402
zoumingxin99 <vitol72@163.com>
2021-01-15 11:45:48 +0800

    完成前端登入注册相关的界面

---
+++ book-mgr-fe/src/router/index.js
@@ -0,0 +1,23 @@
+import { createRouter, createWebHashHistory } from 'vue-router';
+
+
+const routes = [
+  {
+    path: '/auth',
+    name: 'Auth',
+    component: () => import(/* webpackChunkName: "auth" */ '../views/Auth/index.vue'),
+  },
+  // {
+  //   path: '/about',
+  //   name: 'About',
+    
+  //   component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
+  // },
+];
+
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes,
+});
+
+export default router;
---
+++ book-mgr-fe/src/views/Auth/index.vue
@@ -0,0 +1,74 @@
+<template>
+    <div class="auth">
+        <div class="bg"></div>
+        <div class="title-info">
+            <img src="https://ncstatic.clewm.net/rsrc/2020/1016/02/4757e4910cb527fc040d019a93ded74f.png?x-oss-process=image/resize,w_750/format,gif/sharpen,100/quality,Q_80/interlace,1/auto-orient,1" alt>
+            <h2  class="title">图书管理系统 后台</h2>
+        </div>
+
+        <div class="form">
+            <a-tabs >
+                <a-tab-pane tab="登入" key="1">
+                    <div class="item">
+                        <a-input size="large" placeholder="账户" >
+                             <template v-slot:prefix>
+                               <UserOutlined />
+                             </template>
+                        </a-input>   
+                    </div>
+                    <div class="item"> 
+                         <a-input size="large" placeholder="密码" >
+                              <template v-slot:prefix>
+                               <LockOutlined />
+                             </template>
+                         </a-input>
+                    </div>
+                    <div class="item">
+                        <a href="">忘记密码</a>
+                    </div>
+                    <div class="item">
+                        <a-button size="large" type="primary">
+                            登录
+                        </a-button>
+                    </div>
+                </a-tab-pane>
+                <a-tab-pane tab="注册" key="2">
+                   <div class="item">
+                        <a-input size="large" placeholder="账户" >
+                            <template v-slot:prefix>
+                               <UserOutlined />
+                             </template>
+                        </a-input>
+                    </div>
+                    <div class="item"> 
+                         <a-input size="large" placeholder="密码" >
+                             <template v-slot:prefix>
+                               <LockOutlined />
+                             </template>
+                         </a-input>
+                    </div>
+                    <div class="item"> 
+                         <a-input size="large" placeholder="邀请码" >
+                             <template v-slot:prefix>
+                               <MailOutlined />
+                             </template>
+                             
+                         </a-input>
+                    </div>
+                    <div class="item">
+                        <a-button size="large" type="primary">
+                            注册
+                        </a-button>
+                    </div>
+                </a-tab-pane>
+            </a-tabs>
+        </div>
+      
+    </div>
+</template>
+
+<script src = "./index.js"></script>
+
+<style lang ="scss" scpoed>
+    @import './index.scss';
+</style>
