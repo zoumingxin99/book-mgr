b84efd7c85674f7676bd7c3c9cff6ed9cbe1ebe8
zoumingxin99 <vitol72@163.com>
2021-01-18 11:44:29 +0800

    前端图书管理基础界面

--- book-mgr-fe/src/views/Books/index.vue
+++ book-mgr-fe/src/views/Books/index.vue
@@ -1,5 +1,29 @@
 <template>
     <div>
-        books
+        <a-card>
+            <h2>图书列表</h2>
+            <a-divider />
+            
+            <space-between>
+                <div class="search">
+                    <a-input-search  placeholder="根据书名搜索" enter-button />
+                </div>
+
+                <a-button>添加一条</a-button>
+            </space-between>
+
+            <a-divider />
+            <a-table :columns="columns" :data-source="dataSource">
+                
+            </a-table>
+        </a-card>       
     </div>
 </template>
+
+<script src="./index.js">
+
+</script>
+
+<style lang="scss" scoped>
+    @import './index.scss';
+</style>
